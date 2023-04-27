fe9eaa149b87d1c18bacbf2bc6c048e6cd6e96a6
XTsuola <86823660+XTsuola@users.noreply.github.com>
2023-04-27 17:27:21 +0800

    update

--- index.ts
+++ index.ts
@@ -12,7 +12,6 @@ wss.on("connection", function (ws: WebSocketClient) {
   ws.on("message", async function (message: string) {
     if (message != "undefined") {
       const params = JSON.parse(message);
-      console.log(params, "params");
       if (params.connect == true) {
         const ind = idList.findIndex((item: number) => item == params.id);
         if (ind == -1) {
@@ -21,29 +20,54 @@ wss.on("connection", function (ws: WebSocketClient) {
         } else {
           objList.splice(ind, 1, ws);
         }
+        const res2 = await axiod.get(
+          "http://127.0.0.1:7145/qingshu/getGameData",
+        );
+        for (let i = 0; i < idList.length; i++) {
+          objList[i].send(JSON.stringify(res2.data));
+        }
       } else {
-        console.log(params.order, 888)
-        if (params.order == "mopai") { // 摸牌指令
+        if (params.order == "zhunbei") {
           const res = await axiod.get(
-            "http://127.0.0.1:7145/qingshu/mopai?id=" + params.id,
+            "http://127.0.0.1:7145/qingshu/zhunbei?id=" + params.id,
           );
           if (res.data.code == 200) {
-            const res2 = await axiod.get("http://127.0.0.1:7145/qingshu/getGameData");
+            const res2 = await axiod.get(
+              "http://127.0.0.1:7145/qingshu/getGameData",
+            );
             for (let i = 0; i < idList.length; i++) {
               objList[i].send(JSON.stringify(res2.data));
             }
           }
-        } else if(params.order == "reset") { // 重置指令
-          const res = await axiod.get("http://127.0.0.1:7145/qingshu/getGameData");
+        } else if (params.order == "mopai") { // 摸牌指令
+          const res = await axiod.get(
+            "http://127.0.0.1:7145/qingshu/mopai?id=" + params.id,
+          );
+          if (res.data.code == 200) {
+            const res2 = await axiod.get(
+              "http://127.0.0.1:7145/qingshu/getGameData",
+            );
+            for (let i = 0; i < idList.length; i++) {
+              objList[i].send(JSON.stringify(res2.data));
+            }
+          }
+        } else if (params.order == "reset") { // 重置指令
+          const res = await axiod.get(
+            "http://127.0.0.1:7145/qingshu/getGameData",
+          );
           for (let i = 0; i < idList.length; i++) {
             objList[i].send(JSON.stringify(res.data));
           }
-        } else if(params.order == "chupai") { // 出牌指令
+        } else if (params.order == "chupai") { // 出牌指令
           const res = await axiod.get(
-            "http://127.0.0.1:7145/qingshu/chupai?id=" + params.id + "&wz=" + params.wz,
+            "http://127.0.0.1:7145/qingshu/chupai?id=" + params.id + "&wz=" +
+              params.wz + "&tid=" + params.tid + "&tpai=" + params.tpai +
+              "&ypai=" + params.ypai,
           );
           if (res.data.code == 200) {
-            const res2 = await axiod.get("http://127.0.0.1:7145/qingshu/getGameData");
+            const res2 = await axiod.get(
+              "http://127.0.0.1:7145/qingshu/getGameData",
+            );
             for (let i = 0; i < idList.length; i++) {
               objList[i].send(JSON.stringify(res2.data));
             }
